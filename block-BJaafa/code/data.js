f2c85a6b2e9049aa1e2a5ef1949e5f5bda88c34f
Sarang Kulkarni <kulkarnisarang01@gmail.com>
2022-03-28 18:21:56 +0530

    Final Project

---
+++ block-BJaafa/code/data.js
@@ -0,0 +1,42 @@
+let data = [
+    {
+        shortcut: "git config",
+        description: "This command sets the author name and email address respectively to be used with your commits.",
+    },
+    {
+        shortcut: "git init",
+        description: "This command is used to start a new repository.",
+    },
+    {
+        shortcut: "git clone",
+        description: "This command is used to obtain a repository from an existing URL.",
+    },
+    {
+        shortcut: "git add ..",
+        description: "This command adds a file to the staging area.",
+    },
+    {
+        shortcut: "git commit -m “[ Type in the commit message]”",
+        description: "This command records or snapshots the file permanently in the version history.",
+    },
+    {
+        shortcut: "git push [variable name] master ",
+        description: "This command sends the committed changes of master branch to your remote repository.",
+    },
+    {
+        shortcut: "git pull",
+        description: "This command fetches and merges changes on the remote server to your working directory.",
+    },
+    {
+        shortcut: "git stash",
+        description: "This command temporarily stores all the modified tracked files.",
+    },
+    {
+        shortcut: "git remote add [variable name] [Remote Server Link]",
+        description: "This command is used to connect your local repository to the remote server.",
+    },
+    {
+        shortcut: "git checkout",
+        description: "This command sets the author name and email address respectively to be used with your commits",
+    },
+]
